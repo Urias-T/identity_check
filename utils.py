8198c9dd0aeebd9fdbb6e0308bced85e4765030d
Urias-T <triumphurias@gmail.com>
2024-03-08 17:17:23 +0100

    added docstrings fro readability

--- utils.py
+++ utils.py
@@ -2,7 +2,16 @@ import easyocr
 from deepface import DeepFace
 
 
-def name_check(image2_format, name):
+def name_check(image2_format: str, name: str) -> bool:
+    """function to check if name provided matches that on the id
+
+    Args:
+        image2_format (str): format of the id image
+        name (str): name provided
+
+    Returns:
+        bool: boolean: Is the name on the id or not?
+    """
     
     reader = easyocr.Reader(["en"])
     result = reader.readtext(f"temp/id_image.{image2_format}")
@@ -19,7 +28,17 @@ def name_check(image2_format, name):
     return name.lower() in lower_text
 
 
-def compare_faces(image1_format, image2_format):
+def compare_faces(image1_format: str, image2_format: str) -> str:
+    """function to compare the face on the live image to that on the id image using
+    VGG-Face model and cosine similarity.
+
+    Args:
+        image1_format (str): format of the live image
+        image2_format (str): format of the id image
+
+    Returns:
+        str: string indicating match or mismatch
+    """
     
     verification = DeepFace.verify(img1_path=f"temp/live_image.{image1_format}",
                                     img2_path=f"temp/id_image.{image2_format}")
@@ -30,7 +49,18 @@ def compare_faces(image1_format, image2_format):
     return "face_mismatch"
 
 
-def verify_id(image1_format, image2_format, name):
+def verify_id(image1_format: str, image2_format: str, name: str) -> str:
+    """function for verifying that the face on an id matches that on a live image
+    and that the name provided matches that on the id.
+
+    Args:
+        image1_format (str): format of the live image
+        image2_format (str): format of the id image
+        name (str): name provided
+
+    Returns:
+        str: string indicating successful verification or a mismatch
+    """
 
     compare_result = compare_faces(image1_format, image2_format)
 
